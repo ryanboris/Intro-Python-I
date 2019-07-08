917f23c6cf32974f607871a3edb7e347925dc60b
ryanboris <ryan.boris@gmail.com>
2019-07-08 18:34:07 -0400

    :construction: Finish comprehensions, dictionaries, and functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,27 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
-
-# Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
+
+
+def is_even(num):
+    return num % 2 == 0
+
+
+print(is_even(num))
+# Read a number from the keyboard
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 
+
+def is_input_even(num):
+    if num % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd")
+
+
+is_input_even(num)
