9f14ce17e72abc80aeee1d86d23af0db28ec2fa9
ryanboris <ryan.boris@gmail.com>
2019-07-09 17:11:36 -0400

    :sparkles: Complete is_prime function

--- src/prime.py
+++ src/prime.py
@@ -10,3 +10,17 @@ def is_prime(num):
         - all even numbers, except for 2, can't be prime numbers since by definition even numbers would give 0 for %1, %2, and %num
 
     """
+    if num >= 2:
+        for i in range(2, num):
+            if not (num % i):
+                return False
+    else:
+        return False
+    return True
+
+
+print(is_prime(1))
+print(is_prime(2))
+print(is_prime(4))
+print(is_prime(7))
+print(is_prime(-2))
